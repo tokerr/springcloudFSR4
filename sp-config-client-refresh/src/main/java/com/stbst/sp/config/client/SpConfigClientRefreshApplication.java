ec3d9fe77874ad47b36dcda9f0398c6ed41418b2
nongyongtao <nytom@qq.com>
2019-10-21 17:31:18 +0800

    refresh

---
+++ sp-config-client-refresh/src/main/java/com/stbst/sp/config/client/SpConfigClientRefreshApplication.java
@@ -0,0 +1,13 @@
+package com.stbst.sp.config.client;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpConfigClientRefreshApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpConfigClientRefreshApplication.class,args);
+    }
+}
