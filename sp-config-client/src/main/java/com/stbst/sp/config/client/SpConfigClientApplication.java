30f16a41552bef325387f0970174a460737575ab
nongyongtao <nytom@qq.com>
2019-10-21 16:03:27 +0800

    config client

--- sp-config-client/src/main/java/com/stbst/sp/config/client/SpConfigClientApplication.java
+++ sp-config-client/src/main/java/com/stbst/sp/config/client/SpConfigClientApplication.java
@@ -1,4 +1,13 @@
 package com.stbst.sp.config.client;
 
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class SpConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpConfigClientApplication.class,args);
+    }
 }
--- sp-config-client/src/main/java/com/stbst/sp/config/client/controller/ConfigClientController.java
+++ sp-config-client/src/main/java/com/stbst/sp/config/client/controller/ConfigClientController.java
@@ -1,4 +1,19 @@
 package com.stbst.sp.config.client.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ConfigClientController {
+
+
+    @Value("${foo}")
+    private String foo;
+
+    @RequestMapping("/hi")
+    public Object getFoo(){
+
+        return "foo="+foo;
+    }
 }
