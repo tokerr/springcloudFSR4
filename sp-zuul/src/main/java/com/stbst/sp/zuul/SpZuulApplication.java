0206215ebfcfabff6285f6922d439d18316e523a
nongyongtao <nytom@qq.com>
2019-10-22 15:56:05 +0800

    update

---
+++ sp-zuul/src/main/java/com/stbst/sp/zuul/SpZuulApplication.java
@@ -0,0 +1,12 @@
+package com.stbst.sp.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpZuulApplication.class,args);
+    }
+}
