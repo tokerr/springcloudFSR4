9f4b2c6e20e58fe925c12ac2fa2bcfe591075e7c
nongyongtao <nytom@qq.com>
2019-10-22 17:20:06 +0800

    zuul实现自定义过滤器

---
+++ sp-zuul-filter/src/main/java/com/stbst/sp/zuul/filter/MyFilter.java
@@ -0,0 +1,72 @@
+package com.stbst.sp.zuul.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Component
+public class MyFilter extends ZuulFilter {
+
+    private final Logger logger = LoggerFactory.getLogger(MyFilter.class);
+    /**
+     * 过滤器的类型
+     * 返回一个字符串代表的类型，在zuul中定义了四种不同的过滤器类型，贯穿请求的整个生命周期
+     * pre：路由之前
+     * route：路由之时
+     * post：路由之后
+     * error：其他阶段错误时执行该处理器
+     * @return
+     */
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    /**
+     * 过滤的顺序
+     * @return
+     */
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    /**
+     * 是否需要过滤
+     * @return
+     */
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+
+        logger.info("--->>> TokenFilter {},{}", request.getMethod(), request.getRequestURL().toString());
+
+        String token = request.getParameter("token");// 获取请求的参数
+
+        if (StringUtils.isNotBlank(token)) {
+            ctx.setSendZuulResponse(true); //对请求进行路由
+            ctx.setResponseStatusCode(200);
+            ctx.set("isSuccess", true);
+            return null;
+        } else {
+            ctx.setSendZuulResponse(false); //不对其进行路由
+            ctx.setResponseStatusCode(400);
+            ctx.setResponseBody("token is empty");
+            ctx.set("isSuccess", false);
+            return null;
+        }
+    }
+}
