9a118b7da6005cd9aec863c68a27d4001bfa17d0
nongyongtao <nytom@qq.com>
2019-10-22 16:35:12 +0800

    zuul demo

--- sp-zuul/src/main/java/com/stbst/sp/zuul/SpZuulApplication.java
+++ sp-zuul/src/main/java/com/stbst/sp/zuul/SpZuulApplication.java
@@ -2,8 +2,12 @@ package com.stbst.sp.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
 public class SpZuulApplication {
 
     public static void main(String[] args) {
